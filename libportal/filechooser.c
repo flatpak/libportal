9d3e3a0ea7fd3ad0e8fa29803348e437c6372e14
Matthias Clasen <mclasen@redhat.com>
2019-12-17 19:40:40 -0500

    Fix xdp_portal_open_file
    The preconditions were mixed up.

--- libportal/filechooser.c
+++ libportal/filechooser.c
@@ -294,7 +294,7 @@ xdp_portal_open_file (XdpPortal *portal,
   FileCall *call;
 
   g_return_if_fail (XDP_IS_PORTAL (portal));
-  g_return_if_fail (flags & ~(XDP_OPEN_FILE_FLAG_MULTIPLE));
+  g_return_if_fail ((flags & ~(XDP_OPEN_FILE_FLAG_MULTIPLE)) == 0);
 
   call = g_new0 (FileCall, 1);
   call->portal = g_object_ref (portal);
