131db286358ec37213499c3808f13e9941a0bd8f
Bastien Nocera <hadess@hadess.net>
2020-03-18 16:49:53 +0100

    flatpak: Build portal-test from current tree, not github
    Make it easier to test local changes by making the build script build
    the current tree rather than pull sources from github.

--- portal-test/flatpak/build.sh
+++ portal-test/flatpak/build.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
 
-flatpak-builder --force-clean --ccache --repo=repo --install --user app org.gnome.PortalTest.json
+TOP_DIR=`dirname $0`/../..
+OLD_DIR=`pwd`
+
+cd "$TOP_DIR"
+JSON=portal-test/flatpak/org.gnome.PortalTest.json
+# Keep in sync with manifest
+MESON_ARGS="-Dbuild-portal-test=true -Dgtk_doc=false"
+
+rm -rf _flatpak_meson_build
+flatpak-builder --force-clean --ccache --repo=repo --install --user --stop-at=portal-test app $JSON
+flatpak build app meson --prefix=/app --libdir=lib $MESON_ARGS _flatpak_meson_build
+flatpak build app ninja -C _flatpak_meson_build install
+flatpak-builder --finish-only --repo=repo app $JSON
+cd "$OLD_DIR"
