4149e5764fd1c5951434b4278361bf6de81d4976
Bastien Nocera <hadess@hadess.net>
2020-03-18 16:35:55 +0100

    portal-test: Add linking test
    Add a test that mimicks the behaviour of an externally developed
    application when it comes to linking (eg. not sharing libportal's
    internal config.h header).

---
+++ portal-test/portal-linking-test.c
@@ -0,0 +1,12 @@
+#include <libportal/portal-gtk3.h>
+
+int
+main (int argc, char **argv)
+{
+  XdpPortal *portal;
+
+  portal = xdp_portal_new ();
+  (void) xdp_portal_is_camera_present (portal);
+
+  return 0;
+}
