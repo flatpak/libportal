726b4c765928bf734321cb64e6f7ac8cbe6ac1f2
Georges Basile Stavracas Neto <georges.stavracas@gmail.com>
2021-06-17 17:34:13 -0300

    Relicense to LGPL 3.0 only
    So that proprietary applications can link libportal and use
    all the goodness of this platform.
    
    Closes https://github.com/flatpak/libportal/issues/36

--- libportal/background.c
+++ libportal/background.c
@@ -2,18 +2,20 @@
  * Copyright (C) 2018, Matthias Clasen
  * Copyright (C) 2019, Patrick Griffis
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
+ * This file is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, version 3.0 of the
+ * License.
  *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * This file is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with this library. If not, see <http://www.gnu.org/licenses/>.
+ * License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: LGPL-3.0-only
  */
 
 #include "config.h"
