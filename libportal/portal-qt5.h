4c9ae95156953b297d9de9923c6207d2d58d6258
Jan Grulich <jgrulich@redhat.com>
2022-01-06 14:43:20 +0100

    Qt support: add convenient functions to better deal with GVariants
    For Qt users it might be quite challenging to deal with GVariants
    in order to use libportal and this set of functions can conveniently
    avoid that.

--- libportal/portal-qt5.h
+++ libportal/portal-qt5.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020, Jan Grulich
+ * Copyright (C) 2020-2022, Jan Grulich
  *
  * This file is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as
@@ -21,7 +21,89 @@
 
 #include <libportal/portal.h>
 
+#include <QMap>
+#include <QStringList>
+#include <QSharedPointer>
+#include <QVariant>
 #include <QWindow>
 
 XDP_PUBLIC
 XdpParent *xdp_parent_new_qt (QWindow *window);
+
+namespace XdpQt {
+
+// Account portal helpers
+struct GetUserInformationResult {
+    QString id;
+    QString name;
+    QString image;
+};
+
+XDP_PUBLIC
+GetUserInformationResult getUserInformationResultFromGVariant(GVariant *variant);
+
+// FileChooser portal helpers
+enum FileChooserFilterRuleType{
+    Pattern = 0,
+    Mimetype = 1
+};
+
+struct FileChooserFilterRule {
+    FileChooserFilterRuleType type;
+    QString rule;
+};
+
+struct FileChooserFilter {
+    QString label;
+    QList<FileChooserFilterRule> rules;
+};
+
+struct FileChooserChoice {
+    QString id;
+    QString label;
+    QMap<QString, QString> options;
+    QString selected;
+};
+
+XDP_PUBLIC
+GVariant *filechooserFilesToGVariant(const QStringList &files);
+
+XDP_PUBLIC
+GVariant *filechooserFiltersToGVariant(const QList<FileChooserFilter> &filters);
+
+XDP_PUBLIC
+GVariant *filechooserChoicesToGVariant(const QList<FileChooserChoice> &choices);
+
+struct FileChooserResult {
+    QMap<QString, QString> choices;
+    QStringList uris;
+};
+
+XDP_PUBLIC
+FileChooserResult filechooserResultFromGVariant(GVariant *variant);
+
+// Notification portal helpers
+struct NotificationButton {
+    QString label;
+    QString action;
+    QVariant target;
+};
+
+struct Notification {
+    QString title;
+    QString body;
+    QString icon;
+    QPixmap pixmap;
+    QString priority;
+    QString defaultAction;
+    QVariant defaultTarget;
+    QList<NotificationButton> buttons;
+};
+
+XDP_PUBLIC
+GVariant *notificationToGVariant(const Notification &notification);
+
+XDP_PUBLIC
+QVariant GVariantToQVariant(GVariant *variant);
+
+} // namespace XdpQt
---
+++ tests/qt5/test.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2022, Jan Grulich
+ *
+ * This file is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, version 3.0 of the
+ * License.
+ *
+ * This file is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: LGPL-3.0-only
+ */
+
+#ifndef TEST_H
+#define TEST_H
+
+#include <QObject>
+
+class Test : public QObject
+{
+    Q_OBJECT
+private Q_SLOTS:
+    void testFileChooserPortal();
+    void testNotificationPortal();
+};
+
+#endif // TEST_H
