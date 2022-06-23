5da2b357294a029eea2243aa1583795934b40399
Peter Hutterer <peter.hutterer@who-t.net>
2022-06-23 16:04:02 +1000

    tests: add some tests for the ScreenCast portal

---
+++ tests/pyportaltest/templates/screencast.py
@@ -0,0 +1,145 @@
+# SPDX-License-Identifier: LGPL-3.0-only
+#
+# This file is formatted with Python Black
+
+from pyportaltest.templates import Request, Response, ASVType
+from typing import Dict, List, Tuple, Iterator
+from itertools import count
+
+import dbus.service
+import logging
+import socket
+
+logger = logging.getLogger(f"templates.{__name__}")
+
+BUS_NAME = "org.freedesktop.portal.Desktop"
+MAIN_OBJ = "/org/freedesktop/portal/desktop"
+SYSTEM_BUS = False
+MAIN_IFACE = "org.freedesktop.portal.ScreenCast"
+
+_restore_tokens = count()
+
+
+def load(mock, parameters=None):
+    logger.debug(f"loading {MAIN_IFACE} template")
+
+    mock.delay = 500
+    mock.version = parameters.get("version", 4)
+
+    mock.response = parameters.get("response", 0)
+
+    # streams returned in Start
+    mock.streams = parameters.get("streams", [])
+
+    # persist_mode returned in Start
+    mock.persist_mode = parameters.get("persist-mode", 0)
+
+    mock.AddProperties(
+        MAIN_IFACE,
+        dbus.Dictionary(
+            {
+                "version": dbus.UInt32(mock.version),
+                "AvailableSourceTypes": dbus.UInt32(
+                    parameters.get("source-types", 0b111)
+                ),
+                "AvailableCursorModes": dbus.UInt32(
+                    parameters.get("cursor-modes", 0b111)
+                ),
+            }
+        ),
+    )
+
+
+@dbus.service.method(
+    MAIN_IFACE,
+    sender_keyword="sender",
+    in_signature="a{sv}",
+    out_signature="o",
+)
+def CreateSession(self, options, sender):
+    try:
+        logger.debug(f"CreateSession: {options}")
+        request = Request(bus_name=self.bus_name, sender=sender, options=options)
+
+        response = Response(self.response, {})
+
+        request.respond(response, delay=self.delay)
+
+        return request.handle
+    except Exception as e:
+        logger.critical(e)
+
+
+@dbus.service.method(
+    MAIN_IFACE,
+    sender_keyword="sender",
+    in_signature="oa{sv}",
+    out_signature="o",
+)
+def SelectSources(self, session_handle, options, sender):
+    try:
+        logger.debug(f"SelectSources: {session_handle} {options}")
+        request = Request(bus_name=self.bus_name, sender=sender, options=options)
+
+        response = Response(self.response, {})
+
+        request.respond(response, delay=self.delay)
+
+        return request.handle
+    except Exception as e:
+        logger.critical(e)
+
+
+@dbus.service.method(
+    MAIN_IFACE,
+    sender_keyword="sender",
+    in_signature="osa{sv}",
+    out_signature="o",
+)
+def Start(self, session_handle, parent_window, options, sender):
+    try:
+        logger.debug(f"Start: {session_handle} {options}")
+        request = Request(bus_name=self.bus_name, sender=sender, options=options)
+
+        results = {
+            "streams": self.streams,
+        }
+
+        if self.version >= 4:
+            results["persist_mode"] = dbus.UInt32(self.persist_mode)
+            if self.persist_mode != 0:
+                results["restore_token"] = f"restore_token{next(_restore_tokens)}"
+
+        response = Response(self.response, results)
+
+        request.respond(response, delay=self.delay)
+
+        return request.handle
+    except Exception as e:
+        logger.critical(e)
+
+
+@dbus.service.method(
+    MAIN_IFACE,
+    sender_keyword="sender",
+    in_signature="oa{sv}",
+    out_signature="h",
+)
+def OpenPipeWireRemote(self, session_handle, options, sender):
+    try:
+        logger.debug(f"OpenPipeWireRemote: {session_handle} {options}")
+
+        # libportal doesn't care about the socket, so let's use something we
+        # can easily check
+        sockets = socket.socketpair()
+
+        pw_socket = sockets[0]
+        pw_socket.send(b"I AM GROO^WPIPEWIRE")
+
+        fd = sockets[1]
+
+        logger.debug(f"OpenPipeWireRemote with fd {fd.fileno()}")
+
+        return dbus.types.UnixFd(fd)
+    except Exception as e:
+        logger.critical(e)
---
+++ tests/pyportaltest/test_screencast.py
@@ -0,0 +1,270 @@
+# SPDX-License-Identifier: LGPL-3.0-only
+#
+# This file is formatted with Python Black
+
+from . import PortalTest
+
+import dbus
+import gi
+import logging
+import os
+
+from typing import NamedTuple, TextIO
+
+gi.require_version("Xdp", "1.0")
+from gi.repository import GLib, Xdp
+
+logger = logging.getLogger(__name__)
+
+
+class SessionCreationFailed(Exception):
+    def __init__(self, glib_error):
+        self.glib_error = glib_error
+
+    def __str__(self):
+        return f"{type(self).__name__}: {self.glib_error}"
+
+
+class SessionSetup(NamedTuple):
+    session: Xdp.Session
+    pw_fd: TextIO
+
+
+class TestScreenCast(PortalTest):
+    def test_version(self):
+        self.assert_version_eq(4)
+
+    def create_session(
+        self,
+        params=None,
+        parent=None,
+        cancellable=None,
+        outputs=Xdp.OutputType.MONITOR,
+        flags=Xdp.ScreencastFlags.NONE,
+        cursor_mode=Xdp.CursorMode.HIDDEN,
+        persist_mode=Xdp.PersistMode.NONE,
+        restore_token=None,
+    ) -> SessionSetup:
+        self.setup_daemon(params or {})
+
+        xdp = Xdp.Portal.new()
+        assert xdp is not None
+
+        session, session_error = None, None
+        create_session_done_invoked = False
+
+        def create_session_done(portal, task, data):
+            nonlocal session, session_error
+            nonlocal create_session_done_invoked
+
+            create_session_done_invoked = True
+
+            try:
+                session = portal.create_screencast_session_finish(task)
+            except GLib.GError as e:
+                session_error = e
+            self.mainloop.quit()
+
+        xdp.create_screencast_session(
+            outputs=outputs,
+            flags=flags,
+            cursor_mode=cursor_mode,
+            persist_mode=persist_mode,
+            restore_token=restore_token,
+            cancellable=cancellable,
+            callback=create_session_done,
+            data=None,
+        )
+
+        self.mainloop.run()
+        assert create_session_done_invoked
+        if session_error is not None:
+            raise SessionCreationFailed(session_error)
+
+        assert session.get_session_type() == Xdp.SessionType.SCREENCAST
+
+        # open the PW remote by default since we need it anyway for Start()
+        handle = session.open_pipewire_remote()
+        pw_fd = os.fdopen(handle)
+        assert pw_fd.read() == "I AM GROO^WPIPEWIRE"
+
+        return SessionSetup(
+            session=session,
+            pw_fd=pw_fd,
+        )
+
+    def test_create_session(self):
+        """
+        Create a session with some "random" values and ensure that they're
+        passed through to the portal.
+        """
+        outputs = Xdp.OutputType.MONITOR | Xdp.OutputType.WINDOW
+        cursor_mode = Xdp.CursorMode.METADATA
+        persist_mode = Xdp.PersistMode.PERSISTENT
+        restore_token = "12345"
+        flags = Xdp.ScreencastFlags.MULTIPLE
+
+        self.create_session(
+            outputs=outputs,
+            flags=flags,
+            cursor_mode=cursor_mode,
+            restore_token=restore_token,
+            persist_mode=persist_mode,
+        )
+
+        # Now verify our DBus calls were correct
+        method_calls = self.mock_interface.GetMethodCalls("CreateSession")
+        assert len(method_calls) == 1
+        _, args = method_calls.pop(0)
+        (options,) = args
+
+        assert list(options.keys()) == [
+            "handle_token",
+            "session_handle_token",
+        ]
+
+        # This method doesn't exist on the interface anyway but since we share
+        # code paths with RemoteDesktop let's ensure we didn't event try to
+        # call it.
+        method_calls = self.mock_interface.GetMethodCalls("SelectDevices")
+        assert len(method_calls) == 0
+
+        method_calls = self.mock_interface.GetMethodCalls("SelectSources")
+        assert len(method_calls) == 1
+        _, args = method_calls.pop(0)
+        session_handle, options = args
+
+        assert list(options.keys()) == [
+            "handle_token",
+            "types",
+            "multiple",
+            "cursor_mode",
+            "persist_mode",
+            "restore_token",
+        ]
+
+        assert options["types"] == outputs
+        assert options["multiple"] == flags
+        assert options["cursor_mode"] == cursor_mode
+        assert options["restore_token"] == restore_token
+        assert options["persist_mode"] == persist_mode
+
+        method_calls = self.mock_interface.GetMethodCalls("OpenPipeWireRemote")
+        assert len(method_calls) == 1
+        _, args = method_calls.pop(0)
+        session_handle, options = args
+        assert list(options.keys()) == []
+
+    def test_create_session_v3(self):
+        """
+        persist_mode and restore_token were added in v4 of the interface, must
+        not be part of SelectSources
+        """
+        params = {"version": 3}
+
+        persist_mode = Xdp.PersistMode.PERSISTENT
+        restore_token = "12345"
+        self.create_session(
+            params=params,
+            restore_token=restore_token,
+            persist_mode=persist_mode,
+        )
+
+        method_calls = self.mock_interface.GetMethodCalls("SelectSources")
+        assert len(method_calls) == 1
+        _, args = method_calls.pop(0)
+        session_handle, options = args
+
+        assert "persist_mode" not in options.keys()
+        assert "restore_token" not in options.keys()
+
+    def test_session_restore_token_null_if_not_started(self):
+        """
+        Passed in a restore token but unless the session is started it
+        should be NULL.
+        """
+        restore_token = "12345"
+        persist_mode = Xdp.PersistMode.TRANSIENT
+        params = {
+            "persist-mode": persist_mode,
+        }
+        setup = self.create_session(
+            params=params, restore_token=restore_token, persist_mode=persist_mode
+        )
+
+        session = setup.session
+        assert session.get_restore_token() is None
+
+    def test_session_start(self):
+        """
+        Create and start a session
+        """
+
+        s = [
+            {
+                "position": (1, 2),
+                "size": (32, 43),
+                "source_type": dbus.UInt32(Xdp.OutputType.WINDOW),
+            },
+            {
+                "position": (3, 4),
+                "size": (54, 65),
+                "source_type": dbus.UInt32(Xdp.OutputType.MONITOR),
+            },
+        ]
+
+        streams = dbus.Array(
+            [(dbus.UInt32(idx), stream) for idx, stream in enumerate(s)],
+            signature="(ua{sv})",
+            variant_level=1,
+        )
+        params = {
+            "streams": streams,
+            "persist-mode": Xdp.PersistMode.TRANSIENT,
+        }
+
+        restore_token = "foo"  # ignored since we're not continuing a session
+        persist_mode = Xdp.PersistMode.PERSISTENT
+        setup = self.create_session(
+            params=params, restore_token=restore_token, persist_mode=persist_mode
+        )
+
+        session = setup.session
+        start_done_invoked = False
+        start_result, start_error = None, None
+
+        def start_done(portal, task, data):
+            nonlocal start_done_invoked
+            nonlocal start_result, start_error
+
+            start_done_invoked = True
+            try:
+                start_result = session.start_finish(task)
+            except GLib.Error as e:
+                start_error = e
+            self.mainloop.quit()
+
+        session.start(parent=None, cancellable=None, callback=start_done, data=None)
+
+        self.mainloop.run()
+        assert start_done_invoked
+        assert start_error is None
+        assert start_result is True
+
+        # restore_tokenN is what the template uses
+        assert session.get_restore_token() == "restore_token0"
+
+        # FIXME: get_streams() returns a GVariant but how do you dereference that?
+        session_streams = session.get_streams()
+        for s_in, s_out in zip(session_streams, streams):
+            assert s_in == s_out
+
+        # We wanted persistent but we'll get transient from the template
+        assert session.get_persist_mode() == Xdp.PersistMode.TRANSIENT
+
+        method_calls = self.mock_interface.GetMethodCalls("Start")
+        assert len(method_calls) == 1
+        _, args = method_calls.pop(0)
+        session_handle, parent_window, options = args
+        assert parent_window == ""
+        assert list(options.keys()) == ["handle_token"]
