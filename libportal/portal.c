4a0f893a96eba958f91032ce33f9aca543052f00
Peter Hutterer <peter.hutterer@who-t.net>
2022-06-30 14:06:32 +1000

    portal: fix the strcmps on the cgroup hierarchies
    Fixes
    
    ../libportal/portal.c:344:12: warning: logical not is only applied
    to the left hand side of this comparison [-Wlogical-not-parentheses]
               !strcmp (controller, ":") != 0) &&

--- libportal/portal.c
+++ libportal/portal.c
@@ -339,9 +339,10 @@ _xdp_parse_cgroup_file (FILE *f, gboolean *is_snap)
 
       /* Only consider the freezer, systemd group or unified cgroup
        * hierarchies */
-      if ((!strcmp (controller, "freezer:") != 0 ||
-           !strcmp (controller, "name=systemd:") != 0 ||
-           !strcmp (controller, ":") != 0) &&
+      if (controller != NULL &&
+          (g_str_equal (controller, "freezer:") ||
+           g_str_equal (controller, "name=systemd:") ||
+           g_str_equal (controller, ":")) &&
           strstr (cgroup, "/snap.") != NULL)
         {
           *is_snap = TRUE;
