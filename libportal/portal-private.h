f8e5b054d054aba48c5367bd4866c04927cae2a8
Peter Hutterer <peter.hutterer@who-t.net>
2022-08-26 13:50:26 +1000

    Allow overriding the bus name we connect to
    For debugging it's useful to run against a bus that doesn't require
    replacing the real xdg-desktop-portal. Make this possible by setting
    the LIBPORTAL_PORTAL_BUS_NAME environment variable.

--- libportal/portal-private.h
+++ libportal/portal-private.h
@@ -53,7 +53,9 @@ struct _XdpPortal {
   guint screencast_interface_version;
 };
 
-#define PORTAL_BUS_NAME "org.freedesktop.portal.Desktop"
+const char * portal_get_bus_name (void);
+
+#define PORTAL_BUS_NAME (portal_get_bus_name ())
 #define PORTAL_OBJECT_PATH  "/org/freedesktop/portal/desktop"
 #define REQUEST_PATH_PREFIX "/org/freedesktop/portal/desktop/request/"
 #define SESSION_PATH_PREFIX "/org/freedesktop/portal/desktop/session/"
