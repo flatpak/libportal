966edd41bc2a758fcf69a35127b34e60491b32c5
Simon McVittie <smcv@debian.org>
2025-02-10 13:12:12 +0000

    Qt: Use `#pragma once` instead of traditional single-inclusion guards
    This is slightly more efficient, and removes any concerns about whether
    we have reused the same macro name for two different headers.
    
    Signed-off-by: Simon McVittie <smcv@debian.org>

--- tests/qt5/test.h
+++ tests/qt5/test.h
@@ -17,8 +17,7 @@
  * SPDX-License-Identifier: LGPL-3.0-only
  */
 
-#ifndef TEST_H
-#define TEST_H
+#pragma once
 
 #include <QObject>
 
@@ -29,5 +28,3 @@ private Q_SLOTS:
     void testFileChooserPortal();
     void testNotificationPortal();
 };
-
-#endif // TEST_H
--- tests/qt6/test.h
+++ tests/qt6/test.h
@@ -18,8 +18,7 @@
  * SPDX-License-Identifier: LGPL-3.0-only
  */
 
-#ifndef TEST_H
-#define TEST_H
+#pragma once
 
 #include <QObject>
 
@@ -30,5 +29,3 @@ private Q_SLOTS:
     void testFileChooserPortal();
     void testNotificationPortal();
 };
-
-#endif // TEST_H
