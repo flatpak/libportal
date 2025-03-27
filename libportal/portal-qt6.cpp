796053d2eebe4532aad6bd3fd80cdf3b197806ec
Jan Grulich <jgrulich@redhat.com>
2025-03-27 09:38:10 +0100

    qt6: fix build against Qt 6.9+
    QGenericUnixServices was renamed to QDesktopUnixServices in Qt 6.9.
    
    Upstream change: https://codereview.qt-project.org/c/qt/qtbase/+/609639

--- libportal/portal-qt6.cpp
+++ libportal/portal-qt6.cpp
@@ -31,7 +31,11 @@
 #if QT_VERSION >= QT_VERSION_CHECK(6, 5, 0)
 #include <qpa/qplatformintegration.h>
 #include <private/qguiapplication_p.h>
+#if QT_VERSION >= QT_VERSION_CHECK(6, 9, 0)
+#include <private/qdesktopunixservices_p.h>
+#else
 #include <private/qgenericunixservices_p.h>
+#endif
 #endif
 
 static gboolean
@@ -45,7 +49,11 @@ _xdp_parent_export_qt (XdpParent *parent,
   }
 
 #if QT_VERSION >= QT_VERSION_CHECK(6, 5, 0)
+#if QT_VERSION >= QT_VERSION_CHECK(6, 9, 0)
+  if (const auto services = dynamic_cast<QDesktopUnixServices*>(QGuiApplicationPrivate::platformIntegration()->services()))
+#else
   if (const auto services = dynamic_cast<QGenericUnixServices*>(QGuiApplicationPrivate::platformIntegration()->services()))
+#endif
     {
       g_autofree char *handle = g_strdup(services->portalWindowIdentifier(w).toUtf8().constData());
 
