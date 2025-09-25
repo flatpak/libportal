ae033d8627b903864d6e151f0b3be69d39d35dea
Hari Rana <theevilskeleton@riseup.net>
2025-09-25 19:18:49 -0400

    portal: Include `settings.h` header

--- libportal/portal.h
+++ libportal/portal.h
@@ -36,6 +36,7 @@
 #include <libportal/remote.h>
 #include <libportal/screenshot.h>
 #include <libportal/session.h>
+#include <libportal/settings.h>
 #include <libportal/spawn.h>
 #include <libportal/trash.h>
 #include <libportal/types.h>
