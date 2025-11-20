25114a7d4cda4facf8591e805728015e6eda395c
Jonas Ådahl <jadahl@gmail.com>
2025-11-20 16:38:29 +0100

    portal: Keep track of sessions
    This will be necessary when handling signals, which needs to fetch the
    session from a handle.

--- libportal/portal-private.h
+++ libportal/portal-private.h
@@ -30,6 +30,8 @@ struct _XdpPortal {
   GDBusConnection *bus;
   char *sender;
 
+  GHashTable *sessions;
+
   /* inhibit */
   int next_inhibit_id;
   GHashTable *inhibit_handles;
@@ -62,6 +64,15 @@ struct _XdpPortal {
 };
 
 const char * portal_get_bus_name (void);
+
+void xdp_portal_add_session (XdpPortal  *portal,
+                             XdpSession *session);
+
+void xdp_portal_remove_session (XdpPortal  *portal,
+                                XdpSession *session);
+
+XdpSession * xdp_portal_lookup_session (XdpPortal  *portal,
+                                        const char *session_id);
 
 #define PORTAL_BUS_NAME (portal_get_bus_name ())
 #define PORTAL_OBJECT_PATH  "/org/freedesktop/portal/desktop"
